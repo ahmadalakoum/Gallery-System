f6db86fe316b356fb59d55cf1670cccccdf3c242
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-12 10:46:11 +0200

    checking request method

--- server/apis/v1/UserController.php
+++ server/apis/v1/UserController.php
@@ -10,43 +10,51 @@ class UserController
 {
     public static function registerUser()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        $data = json_decode(file_get_contents("php://input"), true);
-        if (empty($data)) {
-            echo json_encode([
-                'status' => 'error',
-                'message' => 'Data is not sent'
-            ]);
-            exit();
-        }
-        //get the data
-        $username = trim($data['username']);
-        $email = trim($data['email']);
-        $password = trim($data['password']);
-        //check for data
-        if (empty($username) || empty($email) || empty($password)) {
-            echo json_encode([
-                'status' => 'error',
-                'message' => 'All fields are required'
-            ]);
-            exit();
-        }
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (empty($data)) {
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Data is not sent'
+                ]);
+                exit();
+            }
+            //get the data
+            $username = trim($data['username']);
+            $email = trim($data['email']);
+            $password = trim($data['password']);
+            //check for data
+            if (empty($username) || empty($email) || empty($password)) {
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'All fields are required'
+                ]);
+                exit();
+            }
 
-        User::create($username, $email, $password);
-        $registered = User::save();
-        if ($registered) {
-            echo json_encode([
-                'status' => 'success',
-                'message' => 'User registered'
-            ]);
-            exit();
+            User::create($username, $email, $password);
+            $registered = User::save();
+            if ($registered) {
+                echo json_encode([
+                    'status' => 'success',
+                    'message' => 'User registered'
+                ]);
+                exit();
+            } else {
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Error during registration'
+                ]);
+                exit();
+
+            }
         } else {
             echo json_encode([
                 'status' => 'error',
-                'message' => 'Error during registration'
+                'message' => 'Wrong request method'
             ]);
             exit();
-
         }
 
 
@@ -55,35 +63,43 @@ class UserController
 
     public static function loginUser()
     {
-        $data = json_decode(file_get_contents("php://input"), true);
-        if (empty($data)) {
-            echo json_encode([
-                'status' => 'error',
-                'message' => 'Data is not sent'
-            ]);
-            exit();
-        }
-        //get the data
-        $email = trim($data['email']);
-        $password = trim($data['password']);
-        //check for data
-        if (empty($email) || empty($password)) {
-            echo json_encode([
-                'status' => 'error',
-                'message' => 'All fields are required'
-            ]);
-            exit();
-        }
-
-        if ($user = User::userExists($email)) {
-            if (password_verify($password, $user['password'])) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (empty($data)) {
                 echo json_encode([
-                    'status' => 'success',
-                    'message' => 'user logged in',
-                    'id' => $user['id'],
-                    'username' => $user['username']
+                    'status' => 'error',
+                    'message' => 'Data is not sent'
                 ]);
                 exit();
+            }
+            //get the data
+            $email = trim($data['email']);
+            $password = trim($data['password']);
+            //check for data
+            if (empty($email) || empty($password)) {
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'All fields are required'
+                ]);
+                exit();
+            }
+
+            if ($user = User::userExists($email)) {
+                if (password_verify($password, $user['password'])) {
+                    echo json_encode([
+                        'status' => 'success',
+                        'message' => 'user logged in',
+                        'id' => $user['id'],
+                        'username' => $user['username']
+                    ]);
+                    exit();
+                } else {
+                    echo json_encode([
+                        'status' => 'error',
+                        'message' => 'Invalid Credentials'
+                    ]);
+                    exit();
+                }
             } else {
                 echo json_encode([
                     'status' => 'error',
@@ -94,7 +110,7 @@ class UserController
         } else {
             echo json_encode([
                 'status' => 'error',
-                'message' => 'Invalid Credentials'
+                'message' => 'Wrong request method'
             ]);
             exit();
         }
